b884053271e142e3168828a54cbf321373b91434
Gillian Scott <gscott@learningexplorer.com>
2024-09-25 15:42:36 -0400

    working on lab2 in class, trying to make an easy navigation for labs

---
+++ src/Labs/Lab2/Float.tsx
@@ -0,0 +1,55 @@
+export default function Float() {
+  return (
+    <div id="wd-float-divs">
+      <h2>Float</h2>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+          Yellow{" "}
+        </div>
+        <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+          Blue{" "}
+        </div>
+        <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+          Red{" "}
+        </div>
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <img
+          className="wd-float-left"
+          width={200}
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <div className="wd-float-done"></div>
+      </div>
+    </div>
+  );
+}
---
+++ src/Labs/Lab2/Style.tsx
@@ -0,0 +1,7 @@
+export default function Style() {
+  return (
+    <div>
+      <h3>Style</h3>
+    </div>
+  );
+}
