873ac5e41a97413a20870ff9c1743909f92329eb
Gillian Scott <gscott@learningexplorer.com>
2024-12-05 18:03:05 -0500

    a6 so far

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -4,6 +4,50 @@ import { useSelector } from "react-redux";
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const active = (path: string) =>
+    pathname.includes(path) ? "active" : "text-danger";
+  const { pathname } = useLocation();
+  return (
+    <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
+      {links.map((link) => (
+        <Link
+          key={link}
+          to={`/Kanbas/Account/${link}`}
+          className={`list-group-item border border-0 ${active(link)}`}
+        >
+          {link}
+        </Link>
+      ))}
+      {currentUser && currentUser.role === "ADMIN" && (
+        <Link
+          to={`/Kanbas/Account/Users`}
+          className={`list-group-item border border-0 ${active("Users")}`}
+        >
+          {" "}
+          Users{" "}
+        </Link>
+      )}
+
+      {/* <Link
+        id="wd-course-people-link"
+        to="/Kanbas/Courses/:cid/People"
+        className="list-group-item text-danger border border-0"
+      >
+        People
+      </Link> */}
+    </div>
+  );
+}
+
+{
+  /*
+  import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+export default function AccountNavigation() {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const active = (path: string) => (pathname.includes(path) ? "active" : "");
   const { pathname } = useLocation();
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
@@ -29,39 +73,8 @@ export default function AccountNavigation() {
           </Link>
         );
       })}
-      {/* <Link
-        id="wd-course-people-link"
-        to="/Kanbas/Courses/:cid/People"
-        className="list-group-item text-danger border border-0"
-      >
-        People
-      </Link> */}
-    </div>
-  );
-}
-
-{
-  /*
-  <Link
-        to={`/Kanbas/Account/Signin`}
-        className="list-group-item active border border-0"
-      >
-        {" "}
-        Signin{" "}
-      </Link>{" "}
-      <Link
-        to={`/Kanbas/Account/Signup`}
-        className="list-group-item text-danger border border-0"
-      >
-        {" "}
-        Signup{" "}
-      </Link>{" "}
-      <Link
-        to={`/Kanbas/Account/Profile`}
-        className="list-group-item text-danger border border-0"
-      >
-        {" "}
-        Profile{" "}
-      </Link>{" "}
+      </div>
+    );
+  }
    */
 }
